8c44d4ed5b6a5275fef51f0c175762fb52c84a8c
ZJHasdf <1061100739@qq.com>
2017-12-25 10:53:20 +0800

    11

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/boxuegu.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/boxuegu.java
@@ -9,5 +9,9 @@ public class boxuegu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boxuegu);
+
+
+
+        //dddd
     }
 }
