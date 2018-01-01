aa596e14bfc2016010c3548dc9544454cee813dc
sailor1024 <1663852293@qq.com>
2018-01-01 23:32:52 +0800

    update lg

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/LoginActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/LoginActivity.java
@@ -62,7 +62,8 @@ public class LoginActivity extends AppCompatActivity {
         tv_find_psw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(LoginActivity.this,FindPswActivity.class);
+                                startActivity(intent);
             }
         });
 
